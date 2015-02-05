a693d578f82f2b2c517b282fc097fdbbd7cac0b8
vektorel <vektorel@lab2-2>
2015-02-05 10:16:02 -0800

    Deneme

---
+++ JavaVektor/src/Adil/Anasinif.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Adil;
+
+/**
+ *
+ * @author vektorel
+ */
+public class Anasinif {
+    public static void main(String[] args) {
+        ders3 ders=new ders3();
+        ders.stringislemleri();
+    }
+    
+}
